dec33b55f89ba59a86f17349cf28e482f8fdf22d
xiarongwen <xiarongwen@learnkg.com>
2025-03-26 14:19:54 +0800

    数据库配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TrainingModule } from './training/training.module';
@@ -16,25 +16,12 @@ import { PassportModule } from '@nestjs/passport';
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-        connectionFactory: (connection) => {
-          connection.on('connected', () => {
-            console.log('MongoDB 已连接');
-          });
-          connection.on('error', (error) => {
-            console.error('MongoDB 连接错误:', error);
-          });
-          return connection;
-        },
-        connectTimeoutMS: 30000,
-        socketTimeoutMS: 45000,
-        serverSelectionTimeoutMS: 30000,
-        retryWrites: true,
-        retryReads: true,
-        maxPoolSize: 50,
-      }),
       inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          uri: configService.get<string>('MONGODB_URI'),
+        } as MongooseModuleOptions;
+      },
     }),
     UsersModule,
     AuthModule,
